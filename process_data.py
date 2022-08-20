d0625c7dd83ec83a3d71c512ceceb7fe1f9fe25a
Xavier Cooney <xavier.cooney03@gmail.com>
2022-08-20 14:05:40 +1000

    Further progress processing data

--- process_data.py
+++ process_data.py
@@ -3,16 +3,19 @@ import csv
 import os
 import sys
 import typing as T
+import pprint
+import json
 
-DATA_DIR = os.path.abspath('data')
+DATA_DIR = os.path.abspath('data_in')
+DATA_OUT = os.path.abspath('data_out')
 
 ELECTION_ID_TO_NAME = {
-    # '27966': '2022 Federal Election',
-    '24310': '2019 Federal Election',
+    '27966': '2022-federal-election',
+    # '24310': '2019-federal-election',
     # '20499': '2016 Federal Election',
 }
 
-STATES = ['ACT']
+STATES = ['ACT', 'NSW', 'NT', 'QLD', 'SA', 'TAS', 'VIC', 'WA']
 NEWLINE = '\r\n'
 
 
@@ -31,6 +34,9 @@ def read_csv_file(csv_path, remove_first_line=True):
 
 
 def normalise_name(given_name, surname):
+    if surname in ('Exhausted', 'Gain/Loss') and given_name == '':
+        return '__' + surname
+
     return f'{surname}, {given_name}'
 
 
@@ -38,19 +44,30 @@ def read_senate_candidate_id_list(election_id):
     candidate_download_path = os.path.join(DATA_DIR, f'SenateCandidatesDownload-{election_id}.csv')
     csv_data = read_csv_file(candidate_download_path)
 
+    colour_csv = read_csv_file(os.path.join('data_in', 'party color codes.csv'))
+    colour_data = {
+        row['party_code']: row['colour']
+        for row in colour_csv
+    }
+
     candidate_name_to_data = {}
 
     for candidate_dict in csv_data:
-        full_name = normalise_name(candidate_dict['Surname'], candidate_dict['GivenNm'])
+        full_name = normalise_name(candidate_dict['GivenNm'], candidate_dict['Surname'])
         assert full_name not in candidate_name_to_data
 
+        party_abbreviation = candidate_dict['PartyAb']
+        if party_abbreviation == '':
+            party_abbreviation = 'IND'
+
         candidate_name_to_data[full_name] = {
-            'party_abbreviation': candidate_dict['PartyAb'],
+            'party_abbreviation': party_abbreviation,
             'party_name': candidate_dict['PartyNm'],
             'candidate_id': candidate_dict['CandidateID'],
             'state': candidate_dict['StateAb'],
             'given_name': candidate_dict['GivenNm'],
             'surname': candidate_dict['Surname'],
+            'colour_data': '#' + colour_data[party_abbreviation]
         }
 
     return candidate_name_to_data
@@ -80,26 +97,61 @@ def compile_dop_data(election_id, state, dop_data, candidate_info):
     rows_by_count = collections.defaultdict(dict)
     election_name = ELECTION_ID_TO_NAME[election_id]
 
+    all_normalised_names = []
+
+    first_row = dop_data[0]
+
     for row in dop_data:
         name = normalise_name(row['GivenNm'], row['Surname'])
         rows_by_count[int(row['Count'])][name] = row
+
+        if name not in all_normalised_names:
+            all_normalised_names.append(name)
 
     final_count_num = max(rows_by_count.keys())
 
     print(f'For {election_name} in {state} there were {final_count_num} counts')
 
     previous_action = {
-        'description': 'First preferences',
-        'candidate': None
+        # 'description': 'First preferences',
+        # 'candidate': None,
     }
 
-    final_data = []
+    # print(all_normalised_names)
+
+    all_count_data = []
 
     for count in range(1, final_count_num + 1):
+        # changed_rows = [
+        #     row
+        #     for row in rows_by_count[count].values()
+        #     if row['Changed']
+        # ]
+
+        # print(count, changed_rows)
+        # assert len(changed_rows) == 1
+
         this_count_data = {
             'count': count,
-            'action': previous_action
+            'progressive_vote_total': {
+                name: int(rows_by_count[count][name]['ProgressiveVoteTotal'])
+                for name in all_normalised_names
+            }
         }
+        all_count_data.append(this_count_data)
+
+    return {
+        'counts': all_count_data,
+        'state': first_row['State'],
+        'num_vacancies': first_row['No Of Vacancies'],
+        'total_formal': first_row['Total Formal Papers'],
+        'quota': first_row['Quota'],
+        'candidate_info': {
+            candidate_name: candidate_data
+            for candidate_name, candidate_data in candidate_info.items()
+            if candidate_data['state'] == state
+        }
+    }
 
 
 def process_election(election_id: str) -> None:
@@ -116,7 +168,9 @@ def process_election(election_id: str) -> None:
 
     for state in STATES:
         dop_data = read_senate_race(election_id, state)
-        compile_dop_data(election_id, state, dop_data, candidate_info)
+        compiled_info = compile_dop_data(election_id, state, dop_data, candidate_info)
+        json_filename = os.path.join(DATA_OUT, f'{election_name}-{state}.json')
+        json.dump(compiled_info, open(json_filename, 'w'))
 
 
 def main() -> None:
@@ -124,5 +178,11 @@ def main() -> None:
         process_election(election_number)
 
 
+def clear_data_out():
+    for filename in os.listdir(DATA_OUT):
+        if filename.endswith('.json'):
+            os.remove(os.path.join(DATA_OUT, filename))
+
 if __name__ == '__main__':
+    clear_data_out()
     main()
